6a49e27512c9f20d0918e7a317624895088c315b
krishna5816 <krishna5816@github.com>
2020-08-07 12:33:49 +0545

    minor update

--- resources/views/users/successapplication.blade.php
+++ resources/views/users/successapplication.blade.php
@@ -16,7 +16,7 @@
 <div class="offset-md-2 col-md-8">
   <div class="card ">
     <div class="card-header">
-      <h5>You Application has been Registered. Please check email.</h5>
+      <h5>You Application has been Registered. Please login.</h5>
     </div>
   </div>
 </div>
